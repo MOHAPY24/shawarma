d430e8bbad05e009edd497de1c08962d3ab10caf
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-10-27 20:47:15 +0400

    added arguments and fixed core

--- src/core.py
+++ src/core.py
@@ -1,4 +1,4 @@
-import json, ollama, os, time
+import json, ollama, os, time, sys
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 
@@ -6,6 +6,12 @@ from prompt_toolkit.history import FileHistory
 session = PromptSession(history=FileHistory('src/configs/memory/.commandhistory.txt'))
 
 
+try:
+    t = True
+    sys.argv[1]
+except:
+    t = False
+    pass
 
 
 
@@ -44,38 +50,43 @@ def write_to_communications(new_data:dict):
 
 def factual_start():
     print(f"[*] Asking model '{factual}' for factual take...")
-    factual_prompt = f"give a factual answer/choice to this text from a user, be detailed too: {user_input}"
+    factual_prompt = f"give a factual/proven/focused answer/choice to this text from a user, be detailed too: {user_input}"
     curr_data.append(client.chat(model=factual, messages=[{"role":"user", "content": factual_prompt}])['message']['content'])
     print(f"[*] Finished asking '{factual}'...")
 
 def creative_start():
     print(f"[*] Asking model '{creative}' for a creative take...")
-    creative_prompt = f"{curr_data}, give a creative answer/choice to this text from a user, you may use the JSON response from a model ment to give a factual response to the said text to use it on your own way, be detailed too: {user_input}"
+    creative_prompt = f"{curr_data}, give a creative/out-of-the-box/intresting answer/choice to this text from a user, you may use the JSON response from a model ment to give a factual response to the said text to use it on your own way, be detailed too: {user_input}"
     curr_data.append(client.chat(model=creative, messages=[{"role":"user", "content": creative_prompt}])['message']['content'])
     print(f"[*] Finished asking '{creative}'...")
 
 def practical_start():
     print(f"[*] Asking model '{practical}' for a practical take...")
-    practical_prompt = f"{curr_data}, give a practical answer/choice to this text from a user, you may use the JSON response from 2 models ment to give a factual and creative choice to said text response to the said text to use it on your own way, be detailed too: {user_input}"
+    practical_prompt = f"{curr_data}, give a practical/consise/conventional answer/choice to this text from a user, you may use the JSON response from 2 models ment to give a factual and creative choice to said text response to the said text to use it on your own way, be detailed too: {user_input}"
     curr_data.append(client.chat(model=practical, messages=[{"role":"user", "content": practical_prompt}])['message']['content'])
     print(f"[*] Finished asking '{practical}'...")
 
 
 while True:
     try:
-        user_input = session.prompt('> ')
+        if t:
+            user_input = sys.argv[1]
+        else:
+            user_input = session.prompt('> ')
         start = time.time()
         factual_start()
         creative_start()
         practical_start()
         print(f"[*] Asking model '{factual}' to merge all responses to one big response...")
-        factual_prompt = f"{curr_data}, using the JSON just passed to you, do not add or modify any information given. Merge all information into one detailed paragraph."
+        factual_prompt = f"{curr_data}, using the JSON just passed to you, do not add or modify any information given. Merge all information into one detailed paragraph. Make it sound like one normal merged paragraph that explains everything."
         response = client.chat(model=factual, messages=[{"role":"user", "content": factual_prompt}])['message']['content']
         curr_data.append(response)
         print(response)
         end = time.time()
         print(f"Elapsed: {(end-time) * 1000:2f} ms")
         write_to_communications(curr_data)
+        if t:
+            t = False
         
         
         
